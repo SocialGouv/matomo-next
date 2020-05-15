db9bc98b87c2140be33615dce264010cd791e623
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-05-15 03:01:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# matomo-next <a href="https://github.com/SocialGouv/matomo-next/workflows/main/"><img src="https://github.com/SocialGouv/matomo-next/workflows/main/badge.svg" alt="Github Master Main Status"></a>
+
+<a href="https://opensource.org/licenses/Apache-2.0"><img src="https://img.shields.io/badge/License-Apache--2.0-yellow.svg" alt="License: Apache-2.0"></a>
+<a href="https://www.npmjs.com/package/@socialgouv/matomo-next"><img src="https://img.shields.io/npm/v/@socialgouv/matomo-next.svg" alt="Npm version"></a>
+
+Matomo analytics for Next.js applications. will track route changes.
+
+## Usage
+
+A sample this to your `_app.js` :
+
+```jsx
+import React from "react";
+import App from "next/app";
+
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
+```
+
+Will track routes changes by default.
+
+You can also track additionnal events :
+
+```js
+import { push } from "@socialgouv/matomo-next";
+
+// track some events
+push(["trackEvent", "contact", "click phone"]);
+```
+
+## Tests
+
+```
+init
+  ✓ should create a js tag and initialize (13 ms)
+push
+  ✓ should append data to window._paq (1 ms)
+router.routeChangeComplete event
+  ✓ should trackPageView with correct title on route change (4 ms)
+  ✓ should use previousPath as referer on consecutive route change (6 ms)
+```
---
+++ src/__tests__/matomo.test.js
@@ -0,0 +1,69 @@
+import { init, push } from "../index";
+import Router from "next/router";
+
+let mockRouteChangeComplete;
+jest.mock("next/router", () => ({
+  query: {},
+  events: {
+    on: (event, cb) => {
+      mockRouteChangeComplete = cb;
+    },
+    emit: (event, route) => {
+      mockRouteChangeComplete(route);
+      jest.fn();
+    },
+  },
+}));
+
+describe("init", () => {
+  beforeEach(() => {
+    global._paq = [];
+  });
+  it("should create a js tag and initialize", () => {
+    // we need to add a fake script node so
+    // init can insert matomo tracker code before it
+    document.head.appendChild(document.createElement("script"));
+    init({ siteId: "42", url: "https://YO" });
+    expect(global._paq).toMatchSnapshot();
+  });
+});
+
+describe("push", () => {
+  test("should append data to window._paq", () => {
+    init({ siteId: "42", url: "YO" });
+    window._paq = [];
+    push(["trackEvent", "kikoo", "lol"]);
+    expect(window._paq).toMatchSnapshot();
+  });
+});
+
+describe("router.routeChangeComplete event", () => {
+  test("should trackPageView with correct title on route change", () => {
+    init({ siteId: "42", url: "YO" });
+    window._paq = [];
+    document.title = "test page";
+
+    Router.events.emit("routeChangeComplete", "/path/to/hello?world");
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        expect(window._paq).toMatchSnapshot();
+        resolve();
+      }, 0);
+    });
+  });
+  test("should use previousPath as referer on consecutive route change", () => {
+    document.title = "test page 2";
+
+    Router.events.emit("routeChangeComplete", "/path/to/hello2?world");
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        expect(window._paq).toMatchSnapshot();
+        resolve();
+      }, 0);
+    });
+  });
+});
+
+// todo: should track pageview on next router routeChangeComplete
