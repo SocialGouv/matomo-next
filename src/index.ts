6280e10328e7392e56fd8ef8c583b3e229d39ceb
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-11 23:39:46 +0200

    fix: dont use deprecated setGenerationTimeMs fix #36

--- src/index.ts
+++ src/index.ts
@@ -100,7 +100,6 @@ export function init({
       push(["setCustomUrl", pathname]);
       push(["setDocumentTitle", document.title]);
       push(["deleteCustomVariables", "page"]);
-      push(["setGenerationTimeMs", 0]);
       if (
         startsWith(pathname, "/recherche") ||
         startsWith(pathname, "/search")
